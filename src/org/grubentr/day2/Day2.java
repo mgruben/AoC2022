af7ae9cb1f76534e2db2c6a1a59248574749ecf1
mgruben <grubenm@gmail.com>
2022-12-18 12:32:31 -0800

    Solve part 1

--- src/org/grubentr/day2/Day2.java
+++ src/org/grubentr/day2/Day2.java
@@ -3,11 +3,15 @@ package org.grubentr.day2;
 import java.util.List;
 
 public class Day2 {
-    public long scoreMatches(List<Match> matches) {
+    public static long scoreMatches(List<Match> matches) {
         long score = 0;
         for (Match match : matches) {
             score += match.score();
         }
         return score;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(scoreMatches(Tokenizer.tokenizeMatches(Data.part1)));
     }
 }
--- test/org/grubentr/day2/TestDay2.java
+++ test/org/grubentr/day2/TestDay2.java
@@ -1,4 +1,14 @@
 package org.grubentr.day2;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TestDay2 {
+
+    @Test
+    public void testTestInput() {
+        long expected = 15L;
+        long actual = Day2.scoreMatches(Tokenizer.tokenizeMatches(Data.testInput));
+        Assertions.assertEquals(expected, actual);
+    }
 }
