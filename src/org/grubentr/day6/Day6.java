e43bbbfa798e0d624880f912eb915fef34fe3670
mgruben <grubenm@gmail.com>
2022-12-26 16:03:29 -0800

    Solve part2

--- src/org/grubentr/day6/Day6.java
+++ src/org/grubentr/day6/Day6.java
@@ -1,9 +1,7 @@
 package org.grubentr.day6;
 
 public class Day6 {
-    public static int part1() {
-        int windowSize = 4;
-
+    public static int findStartOfMessageMarker(int windowSize) {
         CharacterWindow characterWindow = new CharacterWindow(Data.input, windowSize);
 
         while (characterWindow.canAdvance() && characterWindow.getNumUnique() != windowSize) {
@@ -13,7 +11,16 @@ public class Day6 {
         return characterWindow.getNumCharactersProcessed();
     }
 
+    public static int part1() {
+        return findStartOfMessageMarker(4);
+    }
+
+    public static int part2() {
+        return findStartOfMessageMarker(14);
+    }
+
     public static void main(String[] args) {
         System.out.println(part1());
+        System.out.println(part2());
     }
 }
